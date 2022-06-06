a9a0bb02139ccd022af68ca24d4a7d2a3a74c051
Yi <101046167+YiYiYiYiYiYiYiYiYiYiYi@users.noreply.github.com>
2022-06-06 21:27:06 +0200

    Added GitHub url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+    <h1>
     Weather App
+    </h1>
+    <footer>
+      This project was coded by Yi and is {" "}
+    <a Href="https://github.com/YiYiYiYiYiYiYiYiYiYiYi/react-weather-app" target="_blank">
+      open-sourced on GitHub
+
+    </a>
+    </footer>
     </div>
   );
 }
