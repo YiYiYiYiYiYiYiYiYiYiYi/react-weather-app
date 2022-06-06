74ede83481920939fc38e78b671c3cdc69ebd100
Yi <101046167+YiYiYiYiYiYiYiYiYiYiYi@users.noreply.github.com>
2022-06-06 22:12:25 +0200

    added weather section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        
+        <Weather/>
         <footer>
           This project was coded by Yi and is {" "}
           <a Href="https://github.com/YiYiYiYiYiYiYiYiYiYiYi/react-weather-app" target="_blank"> 
